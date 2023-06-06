7ec54ceaca33aaa328768188f34bb51257ef7168
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-06-06 12:31:49 -0400

    Priority by Qualifier o by Name

---
+++ src/main/java/com/nickravch/movierecomender/lesson5/Filter.java
@@ -0,0 +1,5 @@
+package com.nickravch.movierecomender.lesson5;
+
+public interface Filter {
+    public String[] getRecomendations(String movie);
+}
--- src/main/java/com/nickravch/movierecomender/lesson5/RecomenderImplementation.java
+++ src/main/java/com/nickravch/movierecomender/lesson5/RecomenderImplementation.java
@@ -1,12 +1,14 @@
-package com.nickravch.movierecomender.lesson4;
+package com.nickravch.movierecomender.lesson5;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecomenderImplementation {
 
     @Autowired
+    @Qualifier("CF")
     private Filter filter;
 
     public String[] recomendedMovies (String movie) {
