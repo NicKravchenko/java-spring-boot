708806899e3332c6bdb76136a2d562aa2c0b7b1f
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-06-06 15:20:46 -0400

    Tennis-app initial commit

---
+++ tenis-player/src/main/java/com/nickravchenko/tenisplayer/TenisPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.nickravchenko.tenisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisPlayerApplication.class, args);
+	}
+
+}
