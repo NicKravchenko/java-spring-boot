69ef1df83896ac9442e355a6afccacfa49951838
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-06-06 11:53:20 -0400

    Tightly coupled code

---
+++ src/main/java/com/nickravch/movierecomender/lesson1/ContentBasedFilter.java
@@ -0,0 +1,8 @@
+package com.nickravch.movierecomender.lesson1;
+
+public class ContentBasedFilter {
+    public String[] getRecomendations(String movie) {
+        // logic of reccomendation
+        return new String[] {"movie1", "Ice age", "Shark tale"};
+    }
+}
